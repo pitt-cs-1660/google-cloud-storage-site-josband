ac1bf702629fc894a2495cb873287ee20e7b6f18
Jonah Osband <79324007+josband@users.noreply.github.com>
2023-11-11 19:29:46 -0500

    Finished? Probably not

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello! Welcome to my CS 1660 Final Project. - Jonah Osband</p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          H2P
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
